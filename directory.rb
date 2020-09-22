452c2a6f67f40b7405f54d8fae4b8d30f1f24644
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-09-22 16:42:31 +0100

    Directory udpate

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
 # first, we print the list of students
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex Delarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Jofferey Baratheon",
+  "Norman Bates"
+]
+# and the print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Jofferey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 # finally, we print the total number of students
-puts  "Overall, we have #{{student_count}} great students"
+puts  "Overall, we have #{students.count} great students"
